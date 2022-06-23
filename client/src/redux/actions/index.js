8a6e7a3d11294f552eaf62f5edd5cf17a627d7c3
rafacarreno <89260514+rafacarreno@users.noreply.github.com>
2022-06-23 14:27:47 -0300

    add index.js actions

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,14 +1,36 @@
 import axios from 'axios';
 
-export function getProductName(name){
-    return async function (dispatch){
-        try{
-            let json = await axios.get ("http://localhost:3001/product?name=" + name)
+export function getProductName(name) {
+    return async function (dispatch) {
+        try {
+            let json = await axios.get("http://localhost:3001/product?name=" + name)
             return dispatch({
                 type: 'GET_PRODUCT_NAME',
-                payload: json.data 
+                payload: json.data
             })
-        }catch (error){
-             console.log(error)}
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
+
+export function getFilterProducts(filterSelected) {
+
+    return async function (dispatch) {
+        try {
+            const productsByFilter = await axios.get(`http://localhost:3001/filters/:${filterSelected}`);
+            return dispatch({
+                type: 'GET_PRODUCT_BY_FILTER',
+                payload: productsByFilter.data,
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+    export function getOrderProducts(orderSelected) {
+        return {
+            type: 'ORDER_PRODUCTS',
+            payload: orderSelected,
+        }
+    }
