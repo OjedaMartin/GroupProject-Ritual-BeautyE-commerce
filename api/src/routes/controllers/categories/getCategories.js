3b517290d646abd589fe8891b3276d5c48ff54ba
fariasagustin3 <fariasagustin3@gmail.com>
2022-06-24 19:26:34 -0300

    conflicts resoled v2

--- api/src/routes/controllers/categories/getCategories.js
+++ api/src/routes/controllers/categories/getCategories.js
@@ -1,28 +1,4 @@
-const axios = require('axios')
-const {Category} = require('../../../db')
-const apikey = 'fe7a2646d5msh89e07432c535c61p1691f3jsn047b557c3dfd'
-
-// const cargaC = async()=> {
-  
-//     let x= []
-//     let categoria= await axios.get(`https://sephora.p.rapidapi.com/categories/v2/list-root?rapidapi-key=${apikey}`)
-    
-//      categoria.data.rootCategories.map(e => {
-      
-//       let a = {
-//         id: e.categoryId,
-//         name: e.displayName
-//       }
-//       x.push(a)
-  
-//     }); 
-   
-
-                   
-//       }}
-//   }
-
-//   cargaC()
+onst {Category} = require('../../../db')
 
 async function Allcategories(req, res) {
     try {
