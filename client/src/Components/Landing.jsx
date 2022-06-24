88a56108d31ca71e9830626c44590c375fd56135
OjedaMartin <martinignacioojeda@gmail.com>
2022-06-24 15:04:35 -0300

    FIX

--- client/src/Components/Landing.jsx
+++ client/src/Components/Landing.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import Header from './Header';
-import Carousel1 from './Carousel';
 import Footer from './footer';
+import Carousel1 from './Carousel';
 import WhatsNew from './whatsnew';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllProducts } from '../redux/actions';
@@ -11,13 +11,14 @@ export default function Landing(){
 
     const dispatch = useDispatch()
     const products = useSelector((state)=> state.products)
+    
 
     console.log(products)
 
     useEffect(() => {
         dispatch(getAllProducts());
       }, [dispatch]);
-      
+
     return(
         <>
 
