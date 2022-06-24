7abe6038c51acc866835f3804ab1c72be4cabaaa
mariapaulav <mariapaulaviviz@gmail.com>
2022-06-23 20:48:42 -0500

    feat(src/components/homepage) "modificacion"

--- client/src/Components/Landing.jsx
+++ client/src/Components/Landing.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import SearchBar from './SearchBar';
+import SearchBar from './SearchBar'
+import Carousel1 from './Carousel';
+import Footer from './footer';
+import WhatsNew from './whatsnew';
 
 export default function Landing(){
     return(
         <>
             <SearchBar/>
+            <Carousel1/>
+            <WhatsNew/>
+            <Footer/>
         </>
     ) 
 }
