0df5ccb290678b464554a83d284923708ea519d5
fariasagustin3 <fariasagustin3@gmail.com>
2022-06-28 18:39:20 -0300

    comprometiendo archivos para pullear version 3

--- api/src/routes/controllers/products/postProducts.js
+++ api/src/routes/controllers/products/postProducts.js
@@ -2,36 +2,20 @@
 const { Product, Category } = require("../../../db");
 
 const createProduct = async (req, res) => {
-<<<<<<< HEAD
-  const { name, brand, image, price, rating, category, id } = req.body;
-  try {
-      console.log("post", name, brand, image, price, rating, category, id);
-      if(name && brand && image && price && rating && category && id) {
-=======
   const { name, brand, image, price, rating, idcategory } = req.body;
   try {
       console.log("post", name, brand, image, price, rating, idcategory);
       if(name && brand && image && price && rating && idcategory) {
->>>>>>> 76cd64388d31c99c4f8d9b1d9800ea4e4df2cd02
           const newProduct = await Product.create({
               name,
               brand,
               image,
               price,
-<<<<<<< HEAD
-              rating
-          })
-          let productCategory = await Category.findAll({
-              where: {
-                id: id,
-                name: category
-=======
               rating,
           });
           let productCategory = await Category.findOne({
               where: {
                   id: idcategory,
->>>>>>> 76cd64388d31c99c4f8d9b1d9800ea4e4df2cd02
               }
           });
           // console.log(JSON.stringify(newProduct) + " asdasd")
