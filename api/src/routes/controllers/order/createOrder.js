221250a7ba163a4c55487b8b1912074b3d66cf4b
guillo93 <gmllugdar@gmail.com>
2022-07-06 22:50:39 -0300

    arreglos para restar stock y control de un solo carrito en true por user

--- api/src/routes/controllers/order/createOrder.js
+++ api/src/routes/controllers/order/createOrder.js
@@ -1,21 +1,30 @@
-const { Cart,User,Order,CartProduct} = require('../../../db')
+const { Cart,User,Order,CartProduct,Product} = require('../../../db')
 
 
 const createOrder = async(req, res )=>{
     
-    var {email} = req.body
+    var {email,address} = req.body
     var user = await User.findOne({where:{email}}) 
-    
+    let orden
     if (user){
-        let orden= await Order.create({address:user.address})
+        if(!address){
+         orden= await Order.create({address:user.address})
+        }else{ 
+         orden= await Order.create({address})}
         user.addOrder(orden)
-        
+         
         let cart = await Cart.findOne({where:{UserId:user.id,state:"true"}})
         
         if (cart) {
             await Cart.update({state:"false"},{where:{UserId:user.id}})
            await cart.addOrder(orden)
             let prods= await CartProduct.findAll({CartId:cart.id})
+            for (let i = 0; i < prods.length; i++) {
+                let prod =await Product.findOne({where:{id:prods[i].ProductId}}) 
+                let stock = prod.in_Stock - prods[i].quantity
+                await Product.update({in_Stock:stock},{where:{id:prods[i].ProductId}})
+                
+            }
            await orden.addCartProduct(prods)
         }
     }
